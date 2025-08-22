5c616ff2ca99298d20497bec1aeaa901dcd79a84
Nemanja Pesic <adjustrategy@gmail.com>
2025-08-22 14:20:14 +0200

    added DeleteEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Products.GetProductById;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record class DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) 
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+
+        }
+    }
+}
+
+
